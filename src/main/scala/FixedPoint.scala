b0dde2098367ca58dce52d56b97e7abee7fb3013
Tai Huynh <bilun167@gmail.com>
2016-06-19 23:06:55 +0800

    Curry avgDamp func.

--- src/main/scala/FixedPoint.scala
+++ src/main/scala/FixedPoint.scala
@@ -21,7 +21,9 @@ object FixedPoint {
 	}
 
 	def sqrt(x: Double): Double =
-		fixedPoint(y => (y + x / y) / 2)(1)
+		fixedPoint(averageDamp(y => x / y))(1)
+
+	def averageDamp(f: Double => Double)(x: Double) = (x + f(x)) / 2
 
 	def main( args:Array[String] ):Unit = {
 		println(fixedPoint(x => 1 + x / 2)(1))
