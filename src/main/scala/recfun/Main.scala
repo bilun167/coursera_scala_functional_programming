f1d29e92fd4386bc55b58c7a025cc3feec936bd8
Tai Huynh <bilun167@gmail.com>
2016-06-19 19:28:16 +0800

    Implement w1 assignment.

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -1,27 +1,50 @@
 package recfun
 
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
-  }
+	def main(args: Array[String]) {
+		println("Pascal's Triangle")
+		for (row <- 0 to 10) {
+			for (col <- 0 to row)
+				print(pascal(col, row) + " ")
+			println()
+		}
+	}
 
-  /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+	/**
+	  * Exercise 1
+	  */
+	def pascal(c: Int, r: Int): Int = {
+		if (c > r || c < 0 || r < 0) 0
+		else if (c == 0) 1
+		else pascal(c - 1, r - 1) + pascal(c, r - 1)
+	}
+
+	/**
+	  * Exercise 2
+	  */
+	def balance(chars: List[Char]): Boolean = {
+		def step(x: Char): Int = {
+			if (x == ')') -1
+			else if (x == '(') 1
+			else 0
+		}
+
+		def findParanthesis(chars: List[Char], curStep: Int): Boolean = {
+			if (curStep < 0) false
+			else if (chars.isEmpty) curStep == 0
+			else findParanthesis(chars.tail, curStep + step(chars.head))
+		}
+
+		findParanthesis(chars, 0)
+	}
+
+	/**
+	  * Exercise 3
+	  */
+	def countChange(money: Int, coins: List[Int]): Int = {
+		if (money == 0) 1
+		else if (money < 0) 0
+		else if (coins.isEmpty) 0
+		else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+	}
+}
